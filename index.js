5e1a601e49f9f3666e1b7486e101a07d50b1af66
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2024-10-23 10:55:08 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const land = document.getElementById("landooo")
+const btn = document.getElementById("btn")
+const marPfp = document.getElementById("marPfp")
+const amin = document.getElementById("amin")
+
+land.addEventListener("click", function(){
+    window.open("https://adventuretime.fandom.com/wiki/Land_of_Ooo", '_blank')
+})
+
+btn.addEventListener("click", function(){
+    window.open("https://adventuretime.fandom.com/wiki/Category:Characters", "_blank")
+})
+
+marPfp.addEventListener("click", function(){
+    window.open("https://adventuretime.fandom.com/wiki/Marceline", "_blank")
+})
+amin.addEventListener("click", function(){
+    window.open("https://github.com/Aminkhan-mid", "_blank")
+})
